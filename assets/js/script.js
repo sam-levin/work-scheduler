b353c741464f5bc561819a061d192821b8b9016e
sam-levin <sam.ky.levin@gmail.com>
2022-03-07 10:26:55 -0800

    adding time blocks and editing functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 let numHours = 12;
-let numDivisions = 4;
+let numDivisions = 1;
 let minPerDiv = 60/numDivisions;
 let totalDivs = numHours*numDivisions;
 
@@ -13,29 +13,87 @@ var updatedate = function(){
     // this needs to be updated to include moment.js to display current date
 }
 
+var createBlock = function(taskText, taskDate, taskList) {
+    // create elements that make up a task item
+    var taskLi = $("<li>").addClass("list-group-item");
+    var taskSpan = $("<span>")
+      .addClass("badge badge-primary badge-pill")
+      .text(taskDate);
+    var taskP = $("<p>")
+      .addClass("m-1")
+      .text(taskText);
+  
+    // append span and p element to parent li
+    taskLi.append(taskSpan, taskP);
+    auditTask(taskLi)
+  
+    // append to ul list on the page
+    $("#list-" + taskList).append(taskLi);
+  };
+
 
 var buildblocks = function (){
     for (i = 0; i < totalDivs; i ++) {
-        console.log(i)
+        var newBlock = $("<li>").addClass("row")
+        var time = i
+        var timeBlock = $("<h4>")
+            .addClass("block-item col-sm-3")
+            .text(time);
+        var taskBlock = $("<h3>")
+            .addClass("block-item editable col-sm-7")
+        var saveBlock =$("<span>")
+            .addClass("block-item oi oi-plus plus-button col-sm-2")// we need to make this have text align center
+
+        // append to parent list
+        newBlock.append(timeBlock, taskBlock, saveBlock);
+
+        // append to ul list on the page
+        $(".container").append(newBlock);
     }
 }
 
-// for loop to build out blocks of each day
-    // for each 30 min block in the day, build a block
-        // each block has 3 pieces
-        // a time, the main content, and the save button
-        // time and save should be 2 each, with main being 8 widths
-    // blocks should then be given functionality
+// task text was clicked
+$(".container").on("click", "h3", function() {
+    // get current text of p element
+    var text = $(this)
+      .text()
+      .trim();
+  
+    // replace p element with a new textarea
+    var textInput = $("<textarea>").addClass("form-control col-sm-7").val(text);
+    $(this).replaceWith(textInput);
+  
+    // auto focus new element
+    textInput.trigger("focus");
+});
 
-// css of the blocks
-    // they need to be centered
-    // css needs to be dynamic with the time
-    // they do not need to be draggable
+$(".container").on("click", ".plus-button", function() {
+    // get current value of textarea
+    var text = $("textarea").val();
+  
+    // get status type and position in the list
+    /*var status = $(this)
+      .closest(".list-group")
+      .attr("id")
+      .replace("list-", "");
+    var index = $(this)
+      .closest(".list-group-item")
+      .index();
+  
+    // update task in array and re-save to localstorage
+    tasks[status][index].text = text;
+    saveTasks();
+  */
 
-// functionality of the blocks
-    // they need to have bootstrap classes to align them
-    // they need to be clickable
-    // they need to have a save button
+    // recreate p element
+    var taskP = $("<h3>")
+      .addClass("col-sm-7")
+      .text(text);
+  
+    // replace textarea with new content
+    $("textarea").replaceWith(taskP);
+});
+
 
 // there should be an if statement using moment.js timing to determine if a time block has passed
     // if the time is passed, change bootstrap to grey
@@ -46,5 +104,9 @@ var buildblocks = function (){
 updatedate();
 buildblocks();
 
+
+setInterval(function(){
+    updatedate();
+}, 6000)
 // there should be a set interval of 6000 seconds for refreshing the timestamp
 // this can also be used to have the blocks interact with the time stamp here
